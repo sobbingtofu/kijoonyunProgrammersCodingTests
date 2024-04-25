fd09810a39fe7472e5439a18af8cf360855e64d4
sobbingtofu <39695585+sobbingtofu@users.noreply.github.com>
2024-04-25 09:34:05 +0900

    [level 2] Title: 올바른 괄호, Time: 3.50 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s){
+    let count = 0;
+    for(let i=0; i<s.length; i++) {
+        if(s[i] == '(') {
+            count += 1;
+        } else {
+            count -= 1;
+        }
+        if(count < 0) {
+            return false;
+        }
+    }
+
+    return count == 0;
+}
